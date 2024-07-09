62c6ff65f8462242030d505a878b034646c389f3
songhune <songhune@ajou.ac.kr>
2024-07-09 23:35:46 +0900

    입출력 시퀀스를 동적으로 조절

--- train.py
+++ train.py
@@ -76,58 +76,53 @@ tf.app.flags.DEFINE_integer('snapshot_interval', 10000,
 tf.compat.v1.disable_eager_execution()
 
 class Model(object):
-    def __init__(self):
-        # inputs
-        print("flags",FLAGS.batch_size)
+    def __init__(self, input_handle):
+        self.input_handle = input_handle
         self.x = tf.placeholder(tf.float32,
                                 [FLAGS.batch_size,
-                                 FLAGS.seq_length,
-                                 FLAGS.img_width//FLAGS.patch_size,
-                                 FLAGS.img_width//FLAGS.patch_size,
-                                 FLAGS.patch_size*FLAGS.patch_size*FLAGS.img_channel])
+                                 None,  # Use None for dynamic sequence length
+                                 FLAGS.img_width // FLAGS.patch_size,
+                                 FLAGS.img_width // FLAGS.patch_size,
+                                 FLAGS.patch_size * FLAGS.patch_size * FLAGS.img_channel])
 
         self.mask_true = tf.placeholder(tf.float32,
                                         [FLAGS.batch_size,
-                                         FLAGS.seq_length-FLAGS.input_length-1,
-                                         FLAGS.img_width//FLAGS.patch_size,
-                                         FLAGS.img_width//FLAGS.patch_size,
-                                         FLAGS.patch_size*FLAGS.patch_size*FLAGS.img_channel])
+                                         None,  # Use None for dynamic sequence length
+                                         FLAGS.img_width // FLAGS.patch_size,
+                                         FLAGS.img_width // FLAGS.patch_size,
+                                         FLAGS.patch_size * FLAGS.patch_size * FLAGS.img_channel])
 
         grads = []
         loss_train = []
         self.pred_seq = []
         self.tf_lr = tf.placeholder(tf.float32, shape=[])
         num_hidden = [int(x) for x in FLAGS.num_hidden.split(',')]
-        print(num_hidden)
         num_layers = len(num_hidden)
         with tf.compat.v1.variable_scope(tf.get_variable_scope()):
-            # define a model
             output_list = models_factory.construct_model(
                 FLAGS.model_name, self.x,
                 self.mask_true,
                 num_layers, num_hidden,
                 FLAGS.filter_size, FLAGS.stride,
-                FLAGS.seq_length, FLAGS.input_length,
+                None,  # Use None for dynamic sequence length
+                FLAGS.input_length,
                 FLAGS.layer_norm)
             gen_ims = output_list[0]
             loss = output_list[1]
-            pred_ims = gen_ims[:,FLAGS.input_length-1:]
+            pred_ims = gen_ims[:, FLAGS.input_length - 1:]
             self.loss_train = loss / FLAGS.batch_size
-            # gradients
             all_params = tf.trainable_variables()
             grads.append(tf.gradients(loss, all_params))
             self.pred_seq.append(pred_ims)
 
         self.train_op = tf.train.AdamOptimizer(FLAGS.lr).minimize(loss)
-
-        # session
         variables = tf.global_variables()
         self.saver = tf.train.Saver(variables)
         init = tf.global_variables_initializer()
         configProt = tf.ConfigProto()
         configProt.gpu_options.allow_growth = True
         configProt.allow_soft_placement = True
-        self.sess = tf.Session(config = configProt)
+        self.sess = tf.Session(config=configProt)
         self.sess.run(init)
         if FLAGS.pretrained_model:
             self.saver.restore(self.sess, FLAGS.pretrained_model)
@@ -149,7 +144,6 @@ class Model(object):
         checkpoint_path = os.path.join(FLAGS.save_dir, 'model.ckpt')
         self.saver.save(self.sess, checkpoint_path, global_step=itr)
         print('saved to ' + FLAGS.save_dir)
-
 #0326/한승현 save directory for the TESNSORBOARD results
 log_dir = '/home/work/log/mnist_predrnn_pp/'
 os.makedirs(log_dir, exist_ok=True)
@@ -162,7 +156,6 @@ def save_results_to_text_file(file_path, metrics_names, metrics_values):
         writer.writerow(metrics_values)
 
 def main(argv=None):
-    #tf.disable_v2_behavior()
     if tf.gfile.Exists(FLAGS.save_dir):
         tf.gfile.DeleteRecursively(FLAGS.save_dir)
     tf.gfile.MakeDirs(FLAGS.save_dir)
@@ -170,19 +163,17 @@ def main(argv=None):
         tf.gfile.DeleteRecursively(FLAGS.gen_frm_dir)
     tf.gfile.MakeDirs(FLAGS.gen_frm_dir)
 
-    # load data
     train_input_handle, test_input_handle = datasets_factory.data_provider(
         FLAGS.dataset_name, FLAGS.train_data_paths, FLAGS.valid_data_paths,
         FLAGS.batch_size, FLAGS.img_width)
 
     print("Initializing models")
-    model = Model()
+    model = Model(train_input_handle)
     lr = FLAGS.lr
 
     delta = 0.00002
     base = 0.99998
     eta = 1
-    #0326/한승현 TESNSORBOARD writer
     writer = tf.compat.v1.summary.FileWriter(log_dir)
     writer.add_graph(tf.compat.v1.get_default_graph())
     
@@ -190,7 +181,6 @@ def main(argv=None):
         if train_input_handle.no_batch_left():
             train_input_handle.begin(do_shuffle=True)
         ims = train_input_handle.get_batch()
-        #이 코드가 왜 있는가? -> 주어진 이미지를 패치 단위로 처리하려고(전처리하려고...) 하기 때문
         ims = preprocess.reshape_patch(ims, FLAGS.patch_size)
 
         if itr < 50000:
@@ -198,75 +188,70 @@ def main(argv=None):
         else:
             eta = 0.0
         random_flip = np.random.random_sample(
-            (FLAGS.batch_size,FLAGS.seq_length-FLAGS.input_length-1))
+            (FLAGS.batch_size, train_input_handle.current_output_length - 1))
         true_token = (random_flip < eta)
-        #true_token = (random_flip < pow(base,itr))
-        ones = np.ones((FLAGS.img_width//FLAGS.patch_size,
-                        FLAGS.img_width//FLAGS.patch_size,
-                        FLAGS.patch_size**2*FLAGS.img_channel))
-        zeros = np.zeros((FLAGS.img_width//FLAGS.patch_size,
-                          FLAGS.img_width//FLAGS.patch_size,
-                          FLAGS.patch_size**2*FLAGS.img_channel))
+        ones = np.ones((FLAGS.img_width // FLAGS.patch_size,
+                        FLAGS.img_width // FLAGS.patch_size,
+                        FLAGS.patch_size ** 2 * FLAGS.img_channel))
+        zeros = np.zeros((FLAGS.img_width // FLAGS.patch_size,
+                          FLAGS.img_width // FLAGS.patch_size,
+                          FLAGS.patch_size ** 2 * FLAGS.img_channel))
         mask_true = []
         for i in range(FLAGS.batch_size):
-            for j in range(FLAGS.seq_length-FLAGS.input_length-1):
-                if true_token[i,j]:
+            for j in range(train_input_handle.current_output_length - 1):
+                if true_token[i, j]:
                     mask_true.append(ones)
                 else:
                     mask_true.append(zeros)
         mask_true = np.array(mask_true)
         mask_true = np.reshape(mask_true, (FLAGS.batch_size,
-                                           FLAGS.seq_length-FLAGS.input_length-1,
-                                           FLAGS.img_width//FLAGS.patch_size,
-                                           FLAGS.img_width//FLAGS.patch_size,
-                                           FLAGS.patch_size**2*FLAGS.img_channel))
+                                           train_input_handle.current_output_length - 1,
+                                           FLAGS.img_width // FLAGS.patch_size,
+                                           FLAGS.img_width // FLAGS.patch_size,
+                                           FLAGS.patch_size ** 2 * FLAGS.img_channel))
         cost = model.train(ims, lr, mask_true)
         if FLAGS.reverse_input:
-            ims_rev = ims[:,::-1]
+            ims_rev = ims[:, ::-1]
             cost += model.train(ims_rev, lr, mask_true)
-            cost = cost/2
+            cost = cost / 2
 
         if itr % FLAGS.display_interval == 0:
             print('itr: ' + str(itr))
             print('training loss: ' + str(cost))
-          #0326/한승현 tensorboard writer for training loss
             summary = tf.compat.v1.Summary()
             summary.value.add(tag='Training Loss', simple_value=float(cost))
             writer.add_summary(summary, itr)
 
         if itr % FLAGS.test_interval == 0:
             print('test...')
-            test_input_handle.begin(do_shuffle = False)
+            test_input_handle.begin(do_shuffle=False)
             res_path = os.path.join(FLAGS.gen_frm_dir, str(itr))
             os.mkdir(res_path)
             avg_mse = 0
             batch_id = 0
-            img_mse,ssim,psnr,fmae,sharp= [],[],[],[],[]
-            for i in range(FLAGS.seq_length - FLAGS.input_length):
+            img_mse, ssim, psnr, fmae, sharp = [], [], [], [], []
+            for i in range(test_input_handle.current_seq_length - FLAGS.input_length):
                 img_mse.append(0)
                 ssim.append(0)
                 psnr.append(0)
                 fmae.append(0)
                 sharp.append(0)
             mask_true = np.zeros((FLAGS.batch_size,
-                                  FLAGS.seq_length-FLAGS.input_length-1,
-                                  FLAGS.img_width//FLAGS.patch_size,
-                                  FLAGS.img_width//FLAGS.patch_size,
-                                  FLAGS.patch_size**2*FLAGS.img_channel))
-            while(test_input_handle.no_batch_left() == False):
+                                  test_input_handle.current_output_length - 1,
+                                  FLAGS.img_width // FLAGS.patch_size,
+                                  FLAGS.img_width // FLAGS.patch_size,
+                                  FLAGS.patch_size ** 2 * FLAGS.img_channel))
+            while (test_input_handle.no_batch_left() == False):
                 batch_id = batch_id + 1
                 test_ims = test_input_handle.get_batch()
                 test_dat = preprocess.reshape_patch(test_ims, FLAGS.patch_size)
                 img_gen = model.test(test_dat, mask_true)
 
-                # concat outputs of different gpus along batch
                 img_gen = np.concatenate(img_gen)
-                #그러고 나서 여기서 다시 원래 이미지로 복원
                 img_gen = preprocess.reshape_patch_back(img_gen, FLAGS.patch_size)
-                # MSE per frame
-                for i in range(FLAGS.seq_length - FLAGS.input_length):
-                    x = test_ims[:,i + FLAGS.input_length,:,:,0]
-                    gx = img_gen[:,i,:,:,0]
+                for i in range(test_input_handle.current_seq_length - FLAGS.input_length):
+                    x = test_ims[:, i + FLAGS.input_length, :, :, 0]
+                    gx = img_gen[:, i, :, :, 0]
                     fmae[i] += metrics.batch_mae_frame_float(gx, x)
                     gx = np.maximum(gx, 0)
                     gx = np.minimum(gx, 1)
@@ -279,47 +264,46 @@ def main(argv=None):
                     psnr[i] += metrics.batch_psnr(pred_frm, real_frm)
                     for b in range(FLAGS.batch_size):
                         sharp[i] += np.max(
-                            cv2.convertScaleAbs(cv2.Laplacian(pred_frm[b],3)))
-                        score, _ = structural_similarity(pred_frm[b],real_frm[b],full=True)
+                            cv2.convertScaleAbs(cv2.Laplacian(pred_frm[b], 3)))
+                        score, _ = structural_similarity(pred_frm[b], real_frm[b], full=True)
                         ssim[i] += score
 
-                # save prediction examples
                 if batch_id <= 10:
                     path = os.path.join(res_path, str(batch_id))
                     os.mkdir(path)
-                    for i in range(FLAGS.seq_length):
-                        name = 'gt' + str(i+1) + '.png'
+                    for i in range(test_input_handle.current_seq_length):
+                        name = 'gt' + str(i + 1) + '.png'
                         file_name = os.path.join(path, name)
-                        img_gt = np.uint8(test_ims[0,i,:,:,:] * 255)
+                        img_gt = np.uint8(test_ims[0, i, :, :, :] * 255)
                         cv2.imwrite(file_name, img_gt)
-                    for i in range(FLAGS.seq_length-FLAGS.input_length):
-                        name = 'pd' + str(i+1+FLAGS.input_length) + '.png'
+                    for i in range(test_input_handle.current_seq_length - FLAGS.input_length):
+                        name = 'pd' + str(i + 1 + FLAGS.input_length) + '.png'
                         file_name = os.path.join(path, name)
-                        img_pd = img_gen[0,i,:,:,:]
+                        img_pd = img_gen[0, i, :, :, :]
                         img_pd = np.maximum(img_pd, 0)
                         img_pd = np.minimum(img_pd, 1)
                         img_pd = np.uint8(img_pd * 255)
                         cv2.imwrite(file_name, img_pd)
                 test_input_handle.next()
-            avg_mse = avg_mse // (batch_id*FLAGS.batch_size)
+            avg_mse = avg_mse // (batch_id * FLAGS.batch_size)
             print('mse per seq: ' + str(avg_mse))
-            for i in range(FLAGS.seq_length - FLAGS.input_length):
-                print(img_mse[i] // (batch_id*FLAGS.batch_size))
-            psnr = np.asarray(psnr, dtype=np.float32)//batch_id
-            fmae = np.asarray(fmae, dtype=np.float32)//batch_id
-            ssim = np.asarray(ssim, dtype=np.float32)//(FLAGS.batch_size*batch_id)
-            sharp = np.asarray(sharp, dtype=np.float32)//(FLAGS.batch_size*batch_id)
+            for i in range(test_input_handle.current_seq_length - FLAGS.input_length):
+                print(img_mse[i] // (batch_id * FLAGS.batch_size))
+            psnr = np.asarray(psnr, dtype=np.float32) // batch_id
+            fmae = np.asarray(fmae, dtype=np.float32) // batch_id
+            ssim = np.asarray(ssim, dtype=np.float32) // (FLAGS.batch_size * batch_id)
+            sharp = np.asarray(sharp, dtype=np.float32) // (FLAGS.batch_size * batch_id)
             print('psnr per frame: ' + str(np.mean(psnr)))
-            for i in range(FLAGS.seq_length - FLAGS.input_length):
+            for i in range(test_input_handle.current_seq_length - FLAGS.input_length):
                 print(psnr[i])
             print('fmae per frame: ' + str(np.mean(fmae)))
-            for i in range(FLAGS.seq_length - FLAGS.input_length):
+            for i in range(test_input_handle.current_seq_length - FLAGS.input_length):
                 print(fmae[i])
             print('ssim per frame: ' + str(np.mean(ssim)))
-            for i in range(FLAGS.seq_length - FLAGS.input_length):
+            for i in range(test_input_handle.current_seq_length - FLAGS.input_length):
                 print(ssim[i])
             print('sharpness per frame: ' + str(np.mean(sharp)))
-            for i in range(FLAGS.seq_length - FLAGS.input_length):
+            for i in range(test_input_handle.current_seq_length - FLAGS.input_length):
                 print(sharp[i])
             os.makedirs(numeric_dir, exist_ok=True)
             file_name = f'results_itr_{itr}.csv'
@@ -328,7 +312,6 @@ def main(argv=None):
             metrics_values = [psnr.tolist(), fmae.tolist(), ssim.tolist(), sharp.tolist()]
             save_results_to_text_file(file_path, metrics_names, metrics_values)
 
-
         if itr % FLAGS.snapshot_interval == 0:
             model.save(itr)
 
@@ -336,4 +319,3 @@ def main(argv=None):
 
 if __name__ == '__main__':
     tf.app.run()
-
